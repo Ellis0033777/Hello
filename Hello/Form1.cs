0ff45f371908c6072d6536912e424524df38c663
Ellis Stewart <rc0033777@student.riversidecollege.ac.uk>
2018-11-19 15:20:15 +0000

    displAY hello

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -16,5 +16,11 @@ namespace Hello
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello"); 
+
+        }
     }
 }
